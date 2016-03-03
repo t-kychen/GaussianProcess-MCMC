b78aec157e7e1803dc2c47ca871b62a71a2a7c1a
KY Chen <b97501055@gmail.com>
2016-03-02 22:37:51 -0600

    update MCMC algorithm

--- kcMCMC/sdsK.py
+++ kcMCMC/sdsK.py
@@ -13,50 +13,53 @@ import scipy.special
 from scipy.stats import norm
 from kcGP import covK, likK, tools
 
-def elliptical_slice(var, sn):
+def elliptical_slice(f, x, y, hyp):
     """Implementation of elliptical slice sampling
 
     Reference: http://jmlr.csail.mit.edu/proceedings/papers/v9/murray10a/murray10a.pdf
 
-    :param f: initial latent variable
-    :param x: input x
-    :param y: input y
-    :param sn: parameter in likelihood func
+    Parameters
+    ----------
+    f: ndarray with shape (n_samples,)
+        initial latent samples
+    x: ndarray with shape (n_samples, n_features)
+        input samples
+    y: ndarray with shape (n_samples,)
+        input target values
+    hyp: ndarray with shape (3,)
+        hyper-parameters to be updated
 
-    :return ll: log-likelihood
-    :return ff: latent variable
+    Return
+    ------
+
     """
-    f = var[0]
-    x = var[1]
-    y = var[2]
+    nobs = f.shape[0]
     my = np.mean(y)
-    hyp = var[3]
 
-    n = f.shape[0]    
-            
-    Kc    = covK.RBF(np.log(hyp[0]), np.log(hyp[1]))
-    K     = Kc.getCovMatrix(x=x, mode='train')    
+    Kc   = covK.RBF(np.log(hyp[0]), np.log(hyp[1]))
+    K    = Kc.getCovMatrix(x=x, mode='train')
     n_nu = np.random.multivariate_normal(np.zeros_like(f), K, 1)
-    nu = n_nu.T.reshape((n,))
-    
-    lp = -(y-my-f)**2 / sn**2/2 - np.log(2.*np.pi*sn**2)/2. - np.log(sn) - np.log(norm.cdf((4.6-f)/sn) - norm.cdf((0.-f)/sn))
-    cur_logf = lp.sum()
+    nu = n_nu.T.reshape((nobs,))
 
-    log_y = cur_logf + np.log(np.random.uniform())
+    upper = 100. - my
+    lower = 0. - my
+    lik_func = likK.TruncatedGaussNew(upper=upper, lower=lower, log_sigma=np.log(hyp[2]))
+
+    cur_llk = lik_func.evaluate(y=y-my, mu=f)
+    cur_llk = cur_llk + np.log(np.random.uniform())
     
     theta = np.random.uniform(high=2.*np.pi)
     theta_min = theta - 2. * np.pi
     theta_max = theta
     
     # Slice sampling loop
-    while True:  
+    while True:
         prop_f = f * np.cos(theta) + nu * np.sin(theta)
 
-        prop_lp = -(y-my-prop_f)**2 / sn**2/2 - np.log(2.*np.pi*sn**2)/2. - np.log(sn) - np.log(norm.cdf((4.6-prop_f)/sn) - norm.cdf((0.-prop_f)/sn))
-        prop_logf = prop_lp.sum()
+        prop_llk = lik_func.evaluate(y=y-my, mu=prop_f)
 
-        if prop_logf > log_y and np.isfinite(prop_logf):                
-            return prop_logf, prop_f
+        if prop_llk > cur_llk and np.isfinite(prop_llk):
+            return prop_f
 
         else:
             if theta >= 0:
@@ -66,30 +69,33 @@ def elliptical_slice(var, sn):
             
             theta = np.random.uniform(low=theta_min, high=theta_max)
 
-def surrogate_slice_sampling(var, sn, scale):
+def surrogate_slice_sampling(f, x, y, hyp, scale):
     """Implementation of surrogate data slice sampling
 
     Reference: http://papers.nips.cc/paper/4114-slice-sampling-covariance-hyperparameters-of-latent-gaussian-models.pdf
-    
-    var[0] f: ndarray with shape (n_locations,)
+
+    Parameters
+    ----------
+    f: ndarray with shape (n_samples,)
         initial latent samples
-    var[1] x: ndarray with shape (n_samples, n_locations)
-        input samples
-    var[2] y: ndarray with shape (n_locations,)
-        input target values
-    var[3] hyp: ndarray with shape (2,)
-        hyper-parameters to be updated
-    sn: float
-        noise
-    scale: ndarray with shape (n_hyperparams,)
+    x: ndarray with shape (n_samples, n_features)
+        training samples
+    y: ndarray with shape (n_samples,)
+        target values
+    hyp: ndarray with shape (3,)
+        hyper-parameters to be updated, including ll, sf, sn
+    scale: ndarray with shape (n_hyperparameters,)
         proposal ranges of hyper-parameters in SDS sampling update
 
+    Return
+    ----------
+    prop_f: ndarray with shape (n_samples,)
+        updated latent samples
+    prop_hyp: ndarray with shape (3,)
+        updated hyper-parameters, including ll, sf, sn
+
     """
-    f = var[0]
-    x = var[1]
-    y = var[2]
     my = np.mean(y)
-    hyp = np.append(var[3], sn)
 
     Kc = covK.RBF(np.log(hyp[0]), np.log(hyp[1]))
     K  = Kc.getCovMatrix(x=x, mode='train')
@@ -103,22 +109,23 @@ def surrogate_slice_sampling(var, sn, scale):
 
     upper = 100. - my
     lower = 0. - my
-    llk = -(y-my-f)**2 / hyp[2]**2/2. - np.log(2.*np.pi*hyp[2]**2)/2. - np.log(hyp[2]) - np.log(norm.cdf((upper-f)/hyp[2]) - norm.cdf((lower-f)/hyp[2]))
-    curLLK = llk.sum()
+    lik_func = likK.TruncatedGaussNew(upper=upper, lower=lower, log_sigma=np.log(hyp[2]))
+
+    cur_llk = lik_func.evaluate(y=y-my, mu=f)
 
     # curG = np.log(multivariate_normal.pdf(g, np.zeros_like(g), K_S))
     # alpha = tools.solve_chol(L_ks.T, g)
     # curG = -(np.dot(g.T, alpha)/2. + np.log(np.diag(L_ks.T)).sum() + g.shape[0]*np.log(2*np.pi)/2.)
     curG = -(np.dot(np.dot(g.T, np.linalg.inv(K_S)), g)/2. + np.log(np.diag(L_ks.T)).sum() + g.shape[0]*np.log(2*np.pi)/2.)
 
-    k = np.asarray([50., 2., 4.])            # 9, 2, 3
-    theta = np.asarray([0.1, 2., 0.001])     # 0.5, 2, 0.25
-    prior, junk = logGamma(hyp, k, theta, True)
-    threshold = np.log(np.random.uniform()) + curLLK + curG + prior[1] + prior[0] + prior[2]
+    k = np.asarray([55., 5., 3.])        # 55, 5, 3
+    theta = np.asarray([0.1, 2., 5.])     # 0.1, 2.5, 5
+    prior, junk = log_gamma(hyp, k, theta, True)
+    threshold = np.log(np.random.uniform()) + cur_llk + curG + prior[1] + prior[0] + prior[2]
 
     while True:
         prop_hyp = np.random.uniform(low=hyp_min, high=hyp_max)
-        # prop_hyp[2] = sn
+        # prop_hyp[2] = hyp[2]
 
         Kp = covK.RBF(np.log(prop_hyp[0]), np.log(prop_hyp[1]))
         nK = Kp.getCovMatrix(x=x, mode='train')
@@ -126,22 +133,21 @@ def surrogate_slice_sampling(var, sn, scale):
         g, K_S, m_theta_g, chol_R_theta, L_ks = aux_var_model(f, nK, prop_hyp[2], g=g)
         prop_f = np.dot(chol_R_theta, ita) + m_theta_g
 
-        prop_llk = -(y-my-prop_f)**2 / prop_hyp[2]**2/2 - np.log(2.*np.pi*prop_hyp[2]**2)/2. - np.log(prop_hyp[2]) - np.log(norm.cdf((upper-prop_f)/prop_hyp[2]) - norm.cdf((lower-prop_f)/prop_hyp[2]))
-        propLLK = prop_llk.sum()
+        lik_func.sn = prop_hyp[2]
+        prop_llk = lik_func.evaluate(y=y-my, mu=prop_f)
 
         # propG = np.log(multivariate_normal.pdf(g, np.zeros_like(g), K_S))
         # alpha = tools.solve_chol(L_ks.T, g)
         # propG = -(np.dot(g.T, alpha)/2. + np.log(np.diag(L_ks.T)).sum() + g.shape[0]*np.log(2*np.pi)/2.)
         propG = -(np.dot(np.dot(g.T, np.linalg.inv(K_S)), g)/2. + np.log(np.diag(L_ks.T)).sum() + g.shape[0]*np.log(2*np.pi)/2.)
 
-        propPrior, junk = logGamma(prop_hyp, k, theta, True)
-        proposal = propLLK + propG + propPrior[1] + propPrior[0] + propPrior[2]
+        propPrior, junk = log_gamma(prop_hyp, k, theta, True)
+        proposal = prop_llk + propG + propPrior[1] + propPrior[0] + propPrior[2]
 
         if proposal > threshold and np.isfinite(proposal):
-            hyp = prop_hyp
-            # hyp[2] = sn
-            return prop_f, hyp
-        
+            # prop_hyp[2] = hyp[2]
+            return prop_f, prop_hyp
+
         else:
             for i in range(0, 3):
                 if prop_hyp[i] < hyp[i]:
@@ -156,10 +162,16 @@ def aux_var_model(f, K, sn, g=None):
 
     P(f|g,theta) = N(f; m_theta_g, R_theta_g)
 
-    :param f: original latent variables
-    :param K: covariance of f
-    :param alpha: auxiliary noise
-    
+    Parameters
+    ----------
+    f: ndarray with shape (n_samples,)
+        latent samples
+    K: ndarray with shape (n_samples, n_samples)
+        covariance of training samples
+    sn: float
+        noise
+    g: ndarray with shape (n_samples,)
+        auxiliary values with P(g|theta) = N(g; 0, Sigma_theta + S_theta)
     """
     n = K.shape[0]
     Kii = np.diagonal(K)
@@ -187,17 +199,20 @@ def aux_var_model(f, K, sn, g=None):
 
     return g, K+S, m_theta_g, chol_R_theta, L
 
-def logGamma(state, k, theta, invG):
-    '''
-    calculate log pdf of gamma distribution and the gradient of it
-    
-    :param state: hyperparameter to be updated
-    :param k: shape parameter of gamma distribution
-    :param theta: scale parameter of gamma distribution
-    :param invG: whether to compute the prior of noise variance, i.e. inverse Gamma
-    
-    :return: log-likelihood of gamma distribution, its gradient w.r.t. hyperparameters
-    '''
+def log_gamma(state, k, theta, invG):
+    """Log PDF of Gamma and Inverse Gamma distributions and gradients of them
+
+    Parameters
+    ----------
+    state: ndarray with shape (n_hyperparameters,)
+        hyper-parameter to be updated
+    k: ndarray with shape (n_hyperparameters,)
+        shape parameters of gamma & inverse gamma
+    theta: ndarray with shape (n_hyperparameters,)
+        scale parameter of gamma distribution
+    invG: boolean
+        whether to compute the inverse gamma prior
+    """
     logG  = (k-1)*np.log(state) - state/theta - k*np.log(theta) - np.log(scipy.special.gamma(k))                                    # Gamma prior
     gradG = (k-1)*(1/state) - 1/theta
 
@@ -207,14 +222,24 @@ def logGamma(state, k, theta, invG):
 
     return logG, gradG
 
-def infMCMC(xs, sample_f, model, ys=0):
-    '''
-    inference from fs|f
-    '''
+def inf_mcmc(xs, f, model, ys=0):
+    """Inference of fs|f
+
+    Parameters
+    ----------
+    xs: ndarray with shape (n_samples, n_features)
+        testing samples
+    f: ndarray with shape (n_samples, n_mcmc_iters)
+        latent samples from MCMC
+    model: GP instance
+        Trained Gaussian process model, which has x, y, mean, cov & llk func's
+    ys: ndarray with shape (n_samples,)
+        testing target, for the purpose of computing log-likelihood
+    """
     x = model.x
     y = model.y
     my = np.mean(y)
-    n_samples = sample_f.shape[1]
+    n_samples = f.shape[1]
     ns  = xs.shape[0]
 
     n, D = x.shape
@@ -222,7 +247,7 @@ def infMCMC(xs, sample_f, model, ys=0):
     K = model.covfunc.getCovMatrix(x=x, mode='train')
     sn2   = np.exp(2.*model.likfunc.hyp[0])
     L     = tools.jitchol(K/sn2+np.eye(n)).T
-    alpha = tools.solve_chol(L, sample_f-m)/sn2
+    alpha = tools.solve_chol(L, f-m)/sn2
     sW    = np.ones((n, 1))/np.sqrt(sn2)
 
     Ltril = np.all(np.tril(L, -1) == 0)                         # is L an upper triangular matrix?
@@ -240,11 +265,11 @@ def infMCMC(xs, sample_f, model, ys=0):
     fs2 = np.maximum(fs2, 0)
     Fs2 = np.tile(fs2, (1, n_samples))
 
-    trunclik = likK.TruncatedGauss(upper=100.-my, lower=0.-my,
-                                   log_sigma=model.likfunc.hyp[0])
-    lp, Ymu, Lower, Upper = trunclik.evaluateT(ys-my, Fmu[:], Fs2[:])
-    ym  = np.reshape(np.mean(Ymu, axis=1), (ns, 1))
-    ys_lw = np.reshape(np.mean(Lower, axis=1), (ns, 1))
-    ys_up = np.reshape(np.mean(Upper, axis=1), (ns, 1))
+    trunclik = likK.TruncatedGaussNew(upper=100.-my, lower=0.-my,
+                                      log_sigma=model.likfunc.hyp[0])
+    Ymu, Lower, Upper = trunclik.evaluate(mu=Fmu, s2=Fs2)
+    ym  = np.reshape(np.mean(Ymu, axis=1), (ns, 1)) + my
+    ys_lw = np.reshape(np.mean(Lower, axis=1), (ns, 1)) + my
+    ys_up = np.reshape(np.mean(Upper, axis=1), (ns, 1)) + my
 
-    return ym, ys_lw, ys_up, fmu, fs2, lp
+    return ym, ys_lw, ys_up
